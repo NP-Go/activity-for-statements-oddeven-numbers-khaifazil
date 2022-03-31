6dcafdc1d78df5fe1f4e9665a5bfdc8db45f8928
khaifazil <khairal.fazil@gmail.com>
2022-03-31 20:15:08 +0800

    completed activity

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert code here
+	for {
+		var num int
+		fmt.Println("Enter number: ")
+		fmt.Scanln(&num)
+
+		if num%2 != 0 {
+			fmt.Println("Number is Odd!")
+		} else {
+			fmt.Println("Number is Even!")
+		}
+	}
 }
